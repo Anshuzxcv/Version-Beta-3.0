bc4495debe4c23218488334027f4dda9ac9b4adc
amanbharti0302 <amanbharti0302@gmail.com>
2020-10-26 00:40:45 +0530

    upload profile section

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-app.use(express.static("public"));
+var bodyParser = require('body-parser');
+const patientrouter = require('./router/patientrouter');
 
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+
+//user = patient
+app.use('/user',patientrouter);
 
 
 app.get('/',(req,res)=>{
---
+++ router/patientrouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const multer = require('multer');
+var upload = multer();
+
+router.use(upload.array());
+
+router.get('/user-profile',(req,res)=>{
+    res.send('heello');
+})
+
+
+
+
+////////////////////////////////woorking on upload profile section
+router.post('/update-profile',(req,res)=>{
+    const data = req.body;
+    console.log(req.body);
+    res.send('okk');
+})
+
+
+
+module.exports = router;
